6cbc1ebef77240d9517ffe1d34eb06fdcac152b8
tjpo <tjandy.putra@sap.com>
2022-09-22 19:44:47 +0800

    Initial commit

---
+++ comments/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { randomBytes } = require('crypto')
+const axios = require("axios");
+
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const commentsByPostId = {}
+
+
+app.post("/posts/:id/comments", async (req, res) => {
+
+    const commentId = randomBytes(4).toString('hex');
+
+    const { content } = req.body
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content })
+
+    commentsByPostId[req.params.id] = comments
+
+    await axios.post("http://localhost:4005/events", {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
+    res.status(201).send(comments)
+
+})
+
+app.get("/posts/:id/comments", (req, res) => {
+    res.send(commentsByPostId[req.params.id] || [])
+})
+
+
+app.post("/events", (req, res) => {
+    console.log("Received Event ", req.body.type);
+    res.send({})
+})
+
+app.listen(4001, () => { console.log(`Listening on port 4001`) })
